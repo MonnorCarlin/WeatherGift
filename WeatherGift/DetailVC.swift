9cba8e63f566756a68bf526f2839cba9e3e3ad48
Connor Marlin <marlinc@bc.edu>
2019-06-16 15:44:35 -0400

    7.2 and 7.3 completed

---
+++ WeatherGift/DetailVC.swift
@@ -0,0 +1,28 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by Laird Connor Stephen Marlin I, Exquire on 6/16.
+//  Copyright © 2019 Laird Connor Stephen Marlin I, Exquire. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var currentImage: UIImageView!
+    
+    var currentPage = 0
+    var locationsArray = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        locationLabel.text = locationsArray[currentPage]
+    }
+
+}
