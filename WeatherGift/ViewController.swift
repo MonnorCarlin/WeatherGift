6cb7c2104ae7be79ece60f75afe2f3c09981b336
Connor Marlin <marlinc@bc.edu>
2019-06-16 14:26:13 -0400

    Initial Commit

---
+++ WeatherGift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherGift
+//
+//  Created by Laird Connor Stephen Marlin I, Exquire on 6/16.
+//  Copyright © 2019 Laird Connor Stephen Marlin I, Exquire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
