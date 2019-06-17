fbb30eaf073d3fcab0dc110e996a7dfecd94fcbc
Connor Marlin <marlinc@bc.edu>
2019-06-17 18:56:45 -0400

    7.9 completed

--- WeatherGift/AppDelegate.swift
+++ WeatherGift/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        GMSPlacesClient.provideAPIKey("AIzaSyBtYZE0Sl6FwS1urlAkguk6rWqCcgQnEVo")
+        
         return true
     }
 
