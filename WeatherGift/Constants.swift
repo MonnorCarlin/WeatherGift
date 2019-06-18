1932af650b1547dbd5de78a7f1a462a6eec53c2d
Connor Marlin <marlinc@bc.edu>
2019-06-18 11:03:22 -0400

    7.15 completed

---
+++ WeatherGift/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  WeatherGift
+//
+//  Created by Laird Connor Stephen Marlin I, Exquire on 6/18.
+//  Copyright © 2019 Laird Connor Stephen Marlin I, Exquire. All rights reserved.
+//
+
+import Foundation
+
+let urlBase = "https://api.darksky.net/forecast/"
+let urlAPIKey = "a3d5d16af59b1c59d288ded1c8267854" + "/"
